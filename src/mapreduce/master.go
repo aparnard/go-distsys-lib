55e32aba21cad34a335c8a7f3fc490e109126c0c
aparnard <aparna.rd09@gmail.com>
2016-01-16 00:02:25 -0800

    Implementation of master

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -30,5 +30,61 @@ func (mr *MapReduce) KillWorkers() *list.List {
 
 func (mr *MapReduce) RunMaster() *list.List {
 	// Your code here
+
+	//workers already registered in mr.registerChannel
+	//the file is split.
+	//how are the different splits allocated?
+	//func (wk *Worker) DoJob(arg *DoJobArgs, res *DoJobReply)
+
+	fmt.Printf("In Run Master")
+	// Doing Map
+	for i := 0; i < mr.nMap; i++ {
+		
+		go func() {
+			var res DoJobReply
+			res.OK= false
+			wk:=new(Worker)
+			name := <-mr.FreeChannel
+			wk.name =name
+			args := new(DoJobArgs)
+			args.File=mr.file
+			args.Operation="DoMap"
+			args.JobNumber=i
+			args.NumOtherPhase=mr.nReduce
+			
+			for res.OK==false{
+			var err=wk.DoJob(args,&res)
+			if err!=nil {
+				fmt.Printf("Error in Map")
+			}
+			}
+			mr.FreeChannel<-wk.name
+		}()
+	}
+
+	for i := 0; i < mr.nReduce; i++ {
+		
+		go func() {
+			var res DoJobReply
+			res.OK= false
+			wk:=new(Worker)
+			name := <-mr.FreeChannel
+			wk.name = name
+			args := new(DoJobArgs)
+			args.File=mr.file
+			args.Operation="DoReduce"
+			args.JobNumber=i
+			args.NumOtherPhase=mr.nMap
+			
+			for res.OK==false{
+			var err=wk.DoJob(args,&res)
+			if err!=nil {
+				fmt.Printf("Error in Reduce")
+			}
+			}
+			mr.FreeChannel<-wk.name
+		}()
+	}
+	//return 1
 	return mr.KillWorkers()
 }
